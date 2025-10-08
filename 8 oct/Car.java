0c6223057b72615af261df351237670937d01d2c
Rk-D-prince <126610387+Rk-D-prince@users.noreply.github.com>
2025-10-08 18:35:25 +0530

    Add files via upload

---
+++ 8 oct/Car.java
@@ -0,0 +1,17 @@
+class Car
+{
+	public static void main(String[] args)
+	{
+		String carName = "Tesla Model S";
+		String Brand = "Tesla";
+		short mileage =396;
+		String fuelType ="Electric";
+		float price =85000.00f;
+		
+		System.out.println(carName);
+		System.out.println(Brand);
+		System.out.println(fuelType);
+		System.out.println(mileage);
+		System.out.println(price);
+	}
+}
