053dc6b8836cb612a897469543df46b438ed4b1c
Rk-D-prince <126610387+Rk-D-prince@users.noreply.github.com>
2025-10-11 21:54:44 +0530

    Add files via upload

---
+++ 9 oct/Biodata.java
@@ -0,0 +1,20 @@
+class Biodata
+{
+	public static void main(String[] args)
+	{
+		String Name = "Rajakannan";
+		char Initial = 'G';
+		String FatherName = "Govindasamy";
+		String MotherName = "Murugeswari";
+		String District = "Ramanathapuram";
+		byte Date = 9;
+		byte Month = 10;
+		short Year = 2001;
+		
+		System.out.println("My Name is "+Name+"."+Initial);
+		System.out.println("My Father Name is "+FatherName);
+		System.out.println("My Mother Name is "+MotherName);
+		System.out.println("My District is "+District);
+		System.out.println("Date of birth is "+Date+"/"+Month+"/"+Year);
+	}
+}
